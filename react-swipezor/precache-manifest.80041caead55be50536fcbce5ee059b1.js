9f5b6db5e7a4b199c24d128f07e484d1b79f8701
Simran Singh <simran.singh2198@gmail.com>
2020-09-12 20:34:05 +0530

    added react-swipezor demo

---
+++ react-swipezor/precache-manifest.80041caead55be50536fcbce5ee059b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4aa012e08bdc60103e6ef774c79945",
+    "url": "/react-swipezor/index.html"
+  },
+  {
+    "revision": "aa1dd3b29505dca26be9",
+    "url": "/react-swipezor/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b0457d5b9c78e0ae9a7e",
+    "url": "/react-swipezor/static/css/main.610921e0.chunk.css"
+  },
+  {
+    "revision": "aa1dd3b29505dca26be9",
+    "url": "/react-swipezor/static/js/2.38c8fd84.chunk.js"
+  },
+  {
+    "revision": "07c61b0178d42bae60ce67aab1b574a0",
+    "url": "/react-swipezor/static/js/2.38c8fd84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0457d5b9c78e0ae9a7e",
+    "url": "/react-swipezor/static/js/main.7bba1af2.chunk.js"
+  },
+  {
+    "revision": "89261e0b072fe4ace670",
+    "url": "/react-swipezor/static/js/runtime-main.65e7c9a3.js"
+  }
+]);
